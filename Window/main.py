1c2527df2f5ad3b0cad10df2c907ce8b749e8907
Menezess42 <arielmenezess@hotmail.com>
2025-06-16 07:31:09 -0300

    Little by Little I start to understand the pySide6 ploting and how to convert this information to isometric

--- Window/main.py
+++ Window/main.py
@@ -37,14 +37,14 @@ class TransparentWindow(QtWidgets.QWidget):
         )  # Set the parent for all the widgets. In this case all the widgets are childrens of this class
 
         # self.scene = QtWidgets.QGraphicsScene(0, 0, 640, 360)
-        self.scene = QtWidgets.QGraphicsScene(0, 0, 640, 360)
-        self.scene.addRect(0, 0, 640, 360)  # Vai aparecer no canto superior esquerdo do view
+        self.scene = QtWidgets.QGraphicsScene()
+        # self.scene.addRect(0, 0, 640, 360)  # Vai aparecer no canto superior esquerdo do view
 
         # scene = QGraphicsScene(-50, -50, 100, 100)
         # scene.addRect(0, 0, 50, 50)  # O retângulo vai aparecer deslocado (vai precisar ajustar o view)
 
-        self.creating_Tiles(size=2)
-        self.see_Tile_Coordinates()
+        self.creating_Tiles(10)
+        self.creating_char()
 
         view = QtWidgets.QGraphicsView(self.scene)
         view.setInteractive(False)
@@ -65,38 +65,47 @@ class TransparentWindow(QtWidgets.QWidget):
         """
         for line in range(size):
             for column in range(size):
-                block_pixmap = QtGui.QPixmap(f"{ASSETS_PATH}/Blocks/grama32.png")
-                debug_block_pixmap = QtGui.QPixmap(block_pixmap)
+                block_pixmap = QtGui.QPixmap(f"{ASSETS_PATH}/Tests/gramaTile.png")
+                
+                block_item = self.scene.addPixmap(block_pixmap)
 
-                offset_x = TILE_W/2
-                offset_y = TILE_H/2
-
-                painter = QtGui.QPainter(debug_block_pixmap)
-                pen = QtGui.QPen(QtGui.QColor("red"))
-                pen.setWidth(1)
-                painter.setPen(pen)
-                painter.drawEllipse(QtCore.QPointF(offset_x, offset_y), 1, 1)
-                painter.end()
-
-                debug_block_pixmap.save(f"{BASE_DIR}/Window/debug_image.png")
-
-                block_item = self.scene.addPixmap(debug_block_pixmap)
-                block_item.setOffset(offset_x, offset_y)
-
-                x = (column - line) * (TILE_W / 2)
-                y = (column - line) * (TILE_H / 2)
-
+                # x = column * (TILE_W / 2)
+                # y = line * (TILE_H / 2)
+                x = column * 32
+                y = line * 32
                 block_item.setPos(x, y)
+                print(block_item.offset())
 
                 self.grid.append([x, y])
-
+    
     def see_Tile_Coordinates(self):
         for i in self.grid:
-            item = f"{int(i[0])}:{int(i[1])}"
+            item = f"."
             itemScene = self.scene.addText(item)
-            itemScene.setPos(i[0], i[1])
+            itemScene.setPos(i[0], i[1]-16)
             itemScene.setDefaultTextColor("black")
+            
+    def creating_char(self, size: int=0):
+        if size>0:
+            for line in range(size):
+                for column in range(size):
+                    block_pixmap = QtGui.QPixmap(f"{ASSETS_PATH}/Blocks/grama32.png")
+                    block_item = self.scene.addPixmap(block_pixmap)
 
+                    x = column * (TILE_W / 2)
+                    y = line * (TILE_H / 2)
+                    #block_item.setPos(i[0], i[1])
+                    block_item.setPos(x, y)
+                    print(block_item.offset())
+                    print(block_item.pos())
+        else:
+            for i in self.grid:
+                block_pixmap = QtGui.QPixmap(f"{ASSETS_PATH}/Blocks/grama32.png")
+                block_item = self.scene.addPixmap(block_pixmap)
+
+                block_item.setPos(i[0], i[1])
+                print(block_item.offset())
+                print(block_item.pos())
 
 def Frame():
     app = QtWidgets.QApplication([])
